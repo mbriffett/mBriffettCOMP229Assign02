04f50f00b33a42264969bf7d4fbedb658ab5cb2f
mbriffett <matthewbriffett@gmail.com>
2022-10-22 12:25:52 -0230

    more playing with it

--- app/routes/api/contacts-api.route.server.js
+++ app/routes/api/contacts-api.route.server.js
@@ -1,12 +1,17 @@
 //Matthew Briffett 301247484 Centennial College COMP229 Fall 2022
 
 import { Router } from "express";
-import { processLogin, processRegistration, processLogout } from "../../controllers/api/auth-api.controller.server.js";
+import { Add, Delete, Edit, Get, GetList } from "../../controllers/api/contacts-api.controller.server.js";
 
 const router = Router();
 
-router.post('/login', processLogin);
-router.post('/register', processRegistration);
-router.get('/logout', processLogout);
+// REST API VERBS
+
+router.get('/list', GetList);
+router.get('/:id', Get);
+router.post('/add', Add);
+router.put('/edit/:id', Edit);
+router.delete('/delete/:id', Delete);
+
 
 export default router;
